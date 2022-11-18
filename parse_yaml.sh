9b52e67a2008694d9374e4402a69af85f836be07
Brian Smith <bsmith@tcril.org>
2022-11-18 13:52:45 -0500

    make shellcheck happy

--- parse_yaml.sh
+++ parse_yaml.sh
@@ -2,8 +2,8 @@
 #
 # vim: set ft=sh:
 #
-# from: https://gist.github.com/briantjacobs/7753bf850ca5e39be409
-# Based on https://gist.github.com/pkuczynski/8665367
+# adapted from: https://gist.github.com/briantjacobs/7753bf850ca5e39be409
+# which is based on https://gist.github.com/pkuczynski/8665367
 
 parse_yaml() {
     local prefix=$2
@@ -14,7 +14,7 @@ parse_yaml() {
     w='[a-zA-Z0-9_]*'
     fs="$(echo @|tr @ '\034')"
     sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
+        -e "s|^\($s\)\($w\)${s[:-]}$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
     awk -F"$fs" '{
     indent = length($1)/2;
     vname[indent] = $2;
