e0de8a50f6492dcb14322af28c68d5f1f9c717dc
Brian Smith <bsmith@tcril.org>
2022-12-01 16:10:50 -0500

    move some python stuff, start setting up shellspec

--- tests/spec/hello_spec.sh
+++ tests/spec/hello_spec.sh
@@ -1,4 +1,14 @@
 Describe 'hello.sh'
+  setup() { 
+    mkdir bin;
+    cp ../build/atlas ./bin;
+    cp ../build/atlas.yml ./bin;
+  }
+  cleanup() { rm -rf ./bin; }
+  # setup() { echo setup; }
+  # cleanup() { echo cleanup; }
+  BeforeAll 'setup'
+  AfterAll 'cleanup'
   Include lib/hello.sh
   It 'says hello'
     When call hello ShellSpec
