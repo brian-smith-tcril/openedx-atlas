9d763a0e356ba96762c35c5a7f6f33e8722abe2c
Brian Smith <bsmith@tcril.org>
2022-12-13 13:11:57 -0500

    add verbose specs, fix pull specs

--- spec/verbosity_spec.sh
+++ spec/verbosity_spec.sh
@@ -27,6 +27,7 @@ Describe 'Test silent flag'
     The lines of output should equal 0
     The variable GIT_CALLED_QUIETLY should equal true
     The variable GIT_CALLED_NOT_QUIETLY should equal false
+    The variable CP_CALLED should equal true
   End
 
   It 'pulls translations with no output (short flag)'
@@ -37,5 +38,52 @@ Describe 'Test silent flag'
     The lines of output should equal 0
     The variable GIT_CALLED_QUIETLY should equal true
     The variable GIT_CALLED_NOT_QUIETLY should equal false
+    The variable CP_CALLED should equal true
+  End
+End
+
+Describe 'Test verbose flag'
+  # mock pull translations
+  Intercept begin_pull_translations_mock
+  __begin_pull_translations_mock__() {
+    git() {
+      if [ "$quiet" ];
+      then
+        GIT_CALLED_QUIETLY=true
+      else
+        GIT_CALLED_NOT_QUIETLY=true
+      fi
+      %preserve GIT_CALLED_QUIETLY
+      %preserve GIT_CALLED_NOT_QUIETLY
+    }
+
+    cp() {
+      CP_CALLED=true
+      %preserve CP_CALLED
+    }
+  }
+
+  It 'pulls translations with verbose output (full flag)'
+    GIT_CALLED_QUIETLY=false
+    GIT_CALLED_NOT_QUIETLY=false
+    CP_CALLED=false
+    When run source ./atlas pull --verbose
+    The lines of output should equal 14
+    The line 14 of output should equal 'Translations pulled successfully!'
+    The variable GIT_CALLED_QUIETLY should equal false
+    The variable GIT_CALLED_NOT_QUIETLY should equal true
+    The variable CP_CALLED should equal true
+  End
+
+  It 'pulls translations with verbose output (short flag)'
+    GIT_CALLED_QUIETLY=false
+    GIT_CALLED_NOT_QUIETLY=false
+    CP_CALLED=false
+    When run source ./atlas pull -v
+    The lines of output should equal 14
+    The line 14 of output should equal 'Translations pulled successfully!'
+    The variable GIT_CALLED_QUIETLY should equal false
+    The variable GIT_CALLED_NOT_QUIETLY should equal true
+    The variable CP_CALLED should equal true
   End
 End
